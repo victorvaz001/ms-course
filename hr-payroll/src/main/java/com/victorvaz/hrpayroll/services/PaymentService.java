7bb25a40bd383223e0a851fece9ba363b7175464
Victor Vaz <victorvaz001@gmail.com>
2023-08-11 16:19:14 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/victorvaz/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/victorvaz/hrpayroll/services/PaymentService.java
@@ -1,13 +1,32 @@
 package com.victorvaz.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.victorvaz.hrpayroll.entities.Payment;
+import com.victorvaz.hrpayroll.entities.Worker;
 
 @Service //registra a classe como componente do Spring
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPaymenet(Long workerId, int days) {
-		return new Payment("Bob", 200.00, days); //instancia o pagamento mocado
+		
+		/*Mapa de parametros*/
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days); 
 	}
 }
